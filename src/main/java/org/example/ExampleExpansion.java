278d580ca79ac91d37e18b1b3defb18c0df16f36
lako35 <wesleyyu1223@gmail.com>
2025-11-10 21:52:02 -0500

    particle line fix 2

--- src/main/java/org/example/ExampleExpansion.java
+++ src/main/java/org/example/ExampleExpansion.java
@@ -1320,9 +1320,15 @@ plugin = Bukkit.getPluginManager().getPlugin("PlaceholderAPI");
 
 
         if (identifier.startsWith("particleLine_")) {
-            String[] parts = identifier.substring("particleLine_".length()).split(",");
-            ParticleCenterToCenter(UUID.fromString(parts[0]), UUID.fromString(parts[1]));
-            return "Tracking...";
+            try {
+                String[] parts = identifier.substring("particleLine_".length()).split(",");
+
+                if (!"5.0".equals(parts[2])) return "";
+                ParticleCenterToCenter(UUID.fromString(parts[0]), UUID.fromString(parts[1]));
+                return "Tracking...";
+            } catch (Exception e) {
+                return "";
+            }
         }
 
 
